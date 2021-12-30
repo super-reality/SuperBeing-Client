43f795b72728d9946bdb0fcd8b4be22b23642a40
DavinciDreams <lisamegawatts@gmail.com>
2021-12-30 04:54:00 -0500

    Fix missing import

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import axios from "axios";
+import connectionUrl from "./connectionUrl";
 
 const senderName = "Guest";
 
