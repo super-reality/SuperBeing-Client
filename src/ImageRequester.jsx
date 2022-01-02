0771332d5fc95323fcab3e68c672aa6b0f78bc60
alextoti <alexandrostitonis@gmail.com>
2022-01-02 12:07:25 +0200

    added a check to always include a SLASH for the CORS request

--- src/ImageRequester.jsx
+++ src/ImageRequester.jsx
@@ -1,6 +1,6 @@
 export function doCORSRequest(url, printResult) {
     var x = new XMLHttpRequest();
-    x.open('GET', process.env.VITE_SERVER_CORS_URL + url);
+    x.open('GET', (process.env.VITE_SERVER_CORS_URL.endsWith('/') ? process.env.VITE_SERVER_CORS_URL : VITE_SERVER_CORS_URL + '/') + url);
     x.onload = x.onerror = function() {
         printResult(
             JSON.parse(x.responseText).query?.pages[0]?.original.source
