7e3ce63194dcb481efb888e2cb38bf1996079904
alextoti <alexandrostitonis@gmail.com>
2022-01-04 20:48:48 +0200

    Update ImageRequester.jsx

--- src/ImageRequester.jsx
+++ src/ImageRequester.jsx
@@ -2,6 +2,7 @@ export function doCORSRequest(url, printResult) {
     var x = new XMLHttpRequest();
     x.open('GET', (process.env.VITE_SERVER_CORS_URL.endsWith('/') ? process.env.VITE_SERVER_CORS_URL : process.env.VITE_SERVER_CORS_URL + '/') + url);
     x.onload = x.onerror = function() {
+        console.log('cors resp: ' + x.responseText);
         let res = '';
         const json = JSON.parse(x.responseText).query;
         if (json) {
