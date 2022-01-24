faf44c1dfc41a0c7adfb81fe67de7bfead4d835e
alextoti <alexandrostitonis@gmail.com>
2022-01-24 17:18:39 +0200

    added web editor for the profanity system

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,17 @@ import Terminal from "./Terminal";
 import Home from "./Home";
 import AIEditor from "./AIEditor";
 import ConfigEditor from './ConfigEditor';
+import ProfanityEditor from './ProfanityEditor';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Home />} />
         <Route path="/terminal" element={<Terminal />} />
         <Route path="/editor" element={<AIEditor />} />
         <Route path="/config" element={<ConfigEditor />} />
+        <Route path="/"/*profanity"*/ element={<ProfanityEditor />} />
       </Routes>
     </Router>
   );
--- src/ConfigEditor.jsx
+++ src/ConfigEditor.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import AgentEditor from "./AgentEditor";
 
 const ConfigEditor = () => {
   const [firstLoad, setFirstLoad] = useState(true);
