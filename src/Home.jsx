9d9ea6b974b61dbfe3c4c300add80fbeab9130a5
Mandeep-Kaur27 <mandeepgill2718@gmail.com>
2022-04-02 23:38:50 +0530

    worked on super reality website design with resposnive

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,6 @@ import ReactPlayer from "react-player";
 import Chat from "./Chat";
 import { isJson } from "./utils";
 import { v4 as uuidv4 } from "uuid";
-
 export const id = uuidv4();
 export const senderName = "Guest_" + id;
 
@@ -82,149 +81,258 @@ const App = () => {
 
   return (
     <div className="App">
-      <img
-        src="SuperReality_Background.svg"
-        style={{ position: "absolute" }}
-        width="100%"
-        alt="background"
-      />
-      {pageState > 0 && (
-        <div className="ChatWrapper">
-          <Chat
-            agentImage={agentImage}
-            handleClick={() => {
-              window.location.reload(false);
-              setPageState(0);
-            }}
-            agentName={formInputs.agentName}
-            startingMessage={startingMessage}
-          />
-        </div>
-      )}
-      {pageState === 0 && (
-        <div className="joinChatContainer">
-          <img src="/Logo.png" className="logo-big" />
-          <div className="mainInput">
-            <input
-              type="text"
-              placeholder="Who or what do you want to talk to?"
-              name="agentName"
-              value={formInputs.agentName}
-              onKeyPress={(event) => {
-                event.key === "Enter" && startConversation();
+
+      {
+        pageState > 0 && (
+          <div className="ChatWrapper">
+            <Chat
+              agentImage={agentImage}
+              handleClick={() => {
+                window.location.reload(false);
+                setPageState(0);
               }}
-              onChange={onChange}
-            />
-            <button onClick={startConversation} />
-          </div>
-          <div style={{ marginTop: "2em" }}>
-            <b>Try talking to these AIs</b>
-          </div>
-          <div className="ais-container">
-            <AiImage
-              back={treeImage}
-              name="Tree"
-              onClick={() => startConversationFromImage("tree")}
-            />
-            <AiImage
-              back={sunflowerImage}
-              name="Sunflower"
-              onClick={() => startConversationFromImage("sunflower")}
-            />
-            <AiImage
-              back={rabbitImage}
-              name="Rabbit"
-              onClick={() => startConversationFromImage("rabbit")}
-            />
-            <AiImage
-              back={antImage}
-              name="Ant"
-              onClick={() => startConversationFromImage("ant")}
-            />
-            <AiImage
-              back={rainbowImage}
-              name="Rainbow"
-              onClick={() => startConversationFromImage("rainbow")}
-            />
-            <AiImage
-              back={earthImage}
-              name="Earth"
-              onClick={() => startConversationFromImage("earth")}
-            />
-            <AiImage
-              back={socratesImage}
-              name="Socrates"
-              onClick={() => startConversationFromImage("socrates")}
-            />
-            <AiImage
-              back={galileoImage}
-              name="Galileo"
-              onClick={() => startConversationFromImage("galileo")}
-            />
-            <AiImage
-              back={teslaImage}
-              name="Tesla"
-              onClick={() => startConversationFromImage("Nikola Tesla")}
-            />
-            <AiImage
-              back={newtonImage}
-              name="Isaac Newton"
-              onClick={() => startConversationFromImage("Isaac Newton")}
-            />
-            <AiImage
-              back={adaImage}
-              name="Ada Lovelace"
-              onClick={() => startConversationFromImage("Ada Lovelace")}
-            />
-            <AiImage
-              back={rosalindImage}
-              name="Rosalind Franklin"
-              onClick={() => startConversationFromImage("Rosalind Franklin")}
-            />
-            <AiImage
-              back={energyImage}
-              name="Mass-energy equivalence"
-              onClick={() =>
-                startConversationFromImage("Mass-energy equivalence")
-              }
-            />
-            <AiImage
-              back={atomImage}
-              name="Atom"
-              onClick={() => startConversationFromImage("atom")}
-            />
-            <AiImage
-              back={caffeineImage}
-              name="Caffeine"
-              onClick={() => startConversationFromImage("caffeine")}
-            />
-            <AiImage
-              back={cellImage}
-              name="Cell"
-              onClick={() => startConversationFromImage("cell (biology)")}
-            />
-            <AiImage
-              back={sunImage}
-              name="Sun"
-              onClick={() => startConversationFromImage("sun")}
-            />
-            <AiImage
-              back={shakespeareImage}
-              name="Shakespeare"
-              onClick={() => startConversationFromImage("shakespeare")}
+              agentName={formInputs.agentName}
+              startingMessage={startingMessage}
             />
           </div>
-          <ReactPlayer
-            style={{
-              margin: "3em auto",
-              maxWidth: "100%",
-              width: "100%",
-            }}
-            url="https://www.youtube.com/watch?v=Ar54k0sMWe0"
-          />
+        )
+      }
+      <div className="About-company" id="About_Us">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-5 text-center wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="about_img"><img className="img-fluid" src="/src/assets/images/about-us-img.png " /></div>
+            </div>
+            <div className="col-lg-7 common-padding wow fadeInRight animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <h4 className="gradient-text mb-0">About us</h4>
+              <h1 className="black-heading">Our Vision and Mission</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leopulvinar dapibus leo. dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo orem ipsum dolor sit ame orem ipsum dolor sit amet , luctus nec ullamcorper mattis, pulvinar dapibus leo orem ipsum dolor sit ame orem ipsum dolor sit amet</p>
+              <ul className="gradient-arrow list-unstyled mt-4 mb-0 d-flex flex-column">
+                <li><img className="img-fluid" src="/src/assets/images/gradient-arrow.png " /><p>Branding the latest marketing agency</p></li>
+                <li> <img className="img-fluid" src="/src/assets/images/gradient-arrow.png " /><p>Branding the latest marketing agency</p></li>
+                <li> <img className="img-fluid" src="/src/assets/images/gradient-arrow.png " /><p>Branding the latest marketing agency</p></li>
+                <li> <img className="img-fluid" src="/src/assets/images/gradient-arrow.png " /><p>Branding the latest marketing agency</p></li>
+                <li> <img className="img-fluid" src="/src/assets/images/gradient-arrow.png " /><p>Branding the latest marketing agency</p></li>
+              </ul>
+            </div>
+          </div>
         </div>
-      )}
+      </div>
+
+      <div id="Our-AI" className="joinChatContainer text-center common-padding">
+        <div className="container">
+          <div className="row wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+            <div className="col-12">
+              <div className="mainInput">
+                <div className="search-bar">
+                  <input
+                    type="text"
+                    placeholder="Who or what do you want to talk to?"
+                    name="agentName"
+                    value={formInputs.agentName}
+                    onKeyPress={(event) => {
+                      event.key === "Enter" && startConversation();
+                    }}
+                    onChange={onChange}
+                  />
+                  <img className="img-fluid" src="/src/assets/images/gradient-search.png" onClick={startConversation} />
+                </div>
+                <h1 className="black-heading wow fadeInUp animated mb-5" data-wow-duration="1000ms" data-wow-delay="800ms">Try talking to these AIs</h1>
+              </div>
+
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={antImage}
+                  name="Ant"
+                  onClick={() => startConversationFromImage("ant")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={treeImage}
+                  name="Tree"
+                  onClick={() => startConversationFromImage("tree")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={rainbowImage}
+                  name="Rainbow"
+                  onClick={() => startConversationFromImage("rainbow")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={sunflowerImage}
+                  name="Sunflower"
+                  onClick={() => startConversationFromImage("sunflower")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={rabbitImage}
+                  name="Rabbit"
+                  onClick={() => startConversationFromImage("rabbit")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={earthImage}
+                  name="Earth"
+                  onClick={() => startConversationFromImage("earth")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={socratesImage}
+                  name="Socrates"
+                  onClick={() => startConversationFromImage("socrates")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={galileoImage}
+                  name="Galileo"
+                  onClick={() => startConversationFromImage("galileo")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={teslaImage}
+                  name="Tesla"
+                  onClick={() => startConversationFromImage("Nikola Tesla")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={newtonImage}
+                  name="Isaac Newton"
+                  onClick={() => startConversationFromImage("Isaac Newton")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={adaImage}
+                  name="Ada Lovelace"
+                  onClick={() => startConversationFromImage("Ada Lovelace")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={rosalindImage}
+                  name="Rosalind Franklin"
+                  onClick={() => startConversationFromImage("Rosalind Franklin")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={energyImage}
+                  name="Mass-energy equivalence"
+                  onClick={() =>
+                    startConversationFromImage("Mass-energy equivalence")
+                  }
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={atomImage}
+                  name="Atom"
+                  onClick={() => startConversationFromImage("atom")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={caffeineImage}
+                  name="Caffeine"
+                  onClick={() => startConversationFromImage("caffeine")}
+                />
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-6 wow zoomIn animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="ais-container">
+                <AiImage
+                  back={cellImage}
+                  name="Cell"
+                  onClick={() => startConversationFromImage("cell")}
+                />
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div id="Our-Work" className="Our-Work">
+        <div className="container">
+          <div className="row common-padding align-items-center">
+
+            <div className="col-lg-6 text-center wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <div className="video">
+                <ReactPlayer
+                  url="https://www.youtube.com/watch?v=Ar54k0sMWe0"
+                />
+                <img src="/src/assets/images/we-work-img.png" />
+              </div>
+            </div>
+            <div className="col-lg-6 pt-4 pt-lg-0 ps-lg-5 wow fadeInRight animated animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <h4 className="gradient-text mb-0">Videos</h4>
+              <h1 className="black-heading">How we work</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leopulvinar dapibus leo. dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo orem ipsum dolor sit ame orem ipsum dolor sit amet , luctus nec ullamcorper mattis, pulvinar dapibus leo orem ipsum dolor sit ame orem ipsum dolor sit amet</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer className="footer" id="Contact-Us"> 
+      <div className="container">
+        <div className="row common-padding">
+          <h1 className="text-center black-heading wow mb-5 fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="600ms">Connect With Us</h1>
+          <div className="col-lg-3 col-md-6 wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="800ms"><a className="discord" href="#"><img src="/src/assets/images/discord.png "/></a></div>
+          <div className="col-lg-3 col-md-6 wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="1000ms"><a className="fb" href="#"><img src="/src/assets/images/fb.png" /></a></div>
+          <div className="col-lg-3 col-md-6 wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="1200ms"><a className="telegram" href="#"><img src="/src/assets/images/telegram.png" /></a></div>
+          <div className="col-lg-3 col-md-6 wow fadeInDown animated" data-wow-duration="1000ms" data-wow-delay="1400ms"><a className="twitter" href="#"><img src="/src/assets/images/twitter.png" /></a></div>
+  
+        </div>
+      </div>
+    </footer>
+
+    <a href="#" id="back-to-top" title="Back to top">&uarr;</a>
+
     </div>
+
   );
 };
 
--- src/TopNav.jsx
+++ src/TopNav.jsx
@@ -5,18 +5,110 @@ export default function TopNav() {
   const navigate = useNavigate();
 
   return (
-    <div className="top-nav-container">
-      <div className="top-nav-options-container">
-        <div className="top-nav-option" onClick={() => navigate("/admin")}>
-          ADMIN
-        </div>
-        <div className="top-nav-option" onClick={() => navigate("/")}>
-          MEME PAPER
-        </div>
-        <div className="top-nav-option" onClick={() => navigate("/")}>
-          BUY SUPER
+
+    <header className="main-header">
+      <div className="top-bar">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <ul className="header-info d-flex list-unstyled mb-0">
+                <li><a href="#"><i className="fa fa-envelope me-3"></i>Quick Email</a></li>
+              </ul>
+            </div>
+            <div className="col-6 wow fadeInRight animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+              <ul className="top-social-icon d-flex list-unstyled justify-content-end mb-0">
+                <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i className="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="banner" id="#home">
+        <img className="img-fluid" src="src/assets/images/Super-reality-bg.jpg" />
+        <nav className="navbar navbar-expand-lg menu-bar">
+          <div className="container">
+            <div className="row">
+              <div className="menu-wrap d-flex align-items-center">
+                <div className="col-lg-2 full-width">
+                  <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon mb-0"></span>
+                  </button>
+                  <div className="website-logo">
+                    <img src="/src/assets/images/Logo.png" className="img-fluid" alt="logo" />
+                  </div>
+                </div>
+                <div id="navbarCollapse" className="col-lg-10 justify-content-end collapse navbar-collapse">
+                  <div className="navbar-nav flex-links">
+                    <ul className="navigation-links d-flex list-unstyled mb-0 align-items-center">
+                      <li className="nav-item nav-link active">
+                        <a href="#home" className="nav-anchor">
+                          Home
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link" >
+                        <a href="#About_Us" className="nav-anchor">
+                          About Us
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link">
+                        <a href="#Our-AI" className="nav-anchor">
+                          Our AI's
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link">
+                        <a href="#Our-Work" className="nav-anchor">
+                          Videos
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link">
+                        <a href="#" className="nav-anchor">
+                          Meme Paper
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link">
+                        <a href="#" className="nav-anchor">
+                          Buy Super
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link">
+                        <a href="#Contact-Us" className="nav-anchor">
+                          Contact Us
+                        </a>
+                      </li>
+                      <li className="nav-item nav-link Admin">
+                        <a href="#">
+                          Admin
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="text">
+              <div className="col-lg-6 col-md-8 wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+                <div className="text-wrap">
+                  <h1 className="heading gradient-text">super reality</h1>
+                  <h3 className="tagline">future of eductaion</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div className="col-lg-6 col-md-4 wow fadeInLeft animated" data-wow-duration="1000ms" data-wow-delay="600ms">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+    </header>
   );
 }
